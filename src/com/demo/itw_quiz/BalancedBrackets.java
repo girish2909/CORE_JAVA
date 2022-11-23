f788c64d723d1214fccb0f3e7288acb6f0315d35
Girish Mishra <girish.mishra@xebia.com>
2022-11-23 12:50:11 +0530

    code fix

--- src/com/demo/itw_quiz/BalancedBrackets.java
+++ src/com/demo/itw_quiz/BalancedBrackets.java
@@ -1,72 +1,68 @@
 package com.demo.itw_quiz;
+
 // Java program for checking
 // balanced brackets
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class BalancedBrackets {
 
-	// function to check if brackets are balanced
-	static boolean areBracketsBalanced(String expr)
-	{
-		// Using ArrayDeque is faster than using Stack class
-		Deque<Character> stack
-			= new ArrayDeque<Character>();
+    // function to check if brackets are balanced
+    static boolean areBracketsBalanced(String expr) {
+        // Using ArrayDeque is faster than using Stack class
+        Deque<Character> stack = new ArrayDeque<Character>();
 
-		// Traversing the Expression
-		for (int i = 0; i < expr.length(); i++)
-		{
-			char x = expr.charAt(i);
+        // Traversing the Expression
+        for (int i = 0; i < expr.length(); i++) {
+            char x = expr.charAt(i);
 
-			if (x == '(' || x == '[' || x == '{')
-			{
-				// Push the element in the stack
-				stack.push(x);
-				continue;
-			}
+            if (x == '(' || x == '[' || x == '{') {
+                // Push the element in the stack
+                stack.push(x);
+                continue;
+            }
 
-			// If current character is not opening
-			// bracket, then it must be closing. So stack
-			// cannot be empty at this point.
-			if (stack.isEmpty())
-				return false;
-			char check;
-			switch (x) {
-			case ')':
-				check = stack.pop();
-				if (check == '{' || check == '[')
-					return false;
-				break;
+            // If current character is not opening
+            // bracket, then it must be closing. So stack
+            // cannot be empty at this point.
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[')
+                        return false;
+                    break;
 
-			case '}':
-				check = stack.pop();
-				if (check == '(' || check == '[')
-					return false;
-				break;
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[')
+                        return false;
+                    break;
 
-			case ']':
-				check = stack.pop();
-				if (check == '(' || check == '{')
-					return false;
-				break;
-			}
-		}
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{')
+                        return false;
+                    break;
+            }
+        }
 
-		// Check Empty Stack
-		return (stack.isEmpty());
-	}
+        // Check Empty Stack
+        return (stack.isEmpty());
+    }
 
-	// Driver code
-	public static void main(String[] args)
-	{
-		String expr = "";
+    // Driver code
+    public static void main(String[] args) {
+        String expr = "";
 
-		// Function call
-		if(expr.isBlank()){
-			System.out.println("not a valid input");
-		}
-		else if (areBracketsBalanced(expr))
-			System.out.println("Balanced ");
-		else
-			System.out.println("Not Balanced ");
-	}
+        // Function call
+        if (expr.isEmpty()) {
+            System.out.println("not a valid input");
+        } else if (areBracketsBalanced(expr))
+            System.out.println("Balanced ");
+        else
+            System.out.println("Not Balanced ");
+    }
 }
