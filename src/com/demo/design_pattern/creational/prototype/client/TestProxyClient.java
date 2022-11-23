d75ace83c9904032320c3d2d81942fffab683a0d
Girish Mishra <girish.mishra@xebia.com>
2022-11-23 13:08:17 +0530

    new commit

--- src/com/demo/design_pattern/creational/prototype/client/TestProxyClient.java
+++ src/com/demo/design_pattern/creational/prototype/client/TestProxyClient.java
@@ -6,19 +6,24 @@ import com.demo.design_pattern.creational.prototype.Student;
 import com.demo.design_pattern.creational.prototype.StudentDAO;
 
 public class TestProxyClient {
-	public static void main(String[] args) {
-		
-		StudentDAO object = new StudentDAO();
-		List<Student> studentsList = object.getAllStudents();
-		
-		System.out.println("Original Students List ::");
-		studentsList.forEach(System.out::println);
-		System.out.println("----------------------------");
-		List<Student> dummyStudents = object.clone();
-		Student std = new Student();
-		std.setName("vineet");
-		std.setStudendId("1003");
-		dummyStudents.add(std);
-		dummyStudents.forEach(System.out::println);
-	}
+    public static void main(String[] args) {
+
+        StudentDAO object = new StudentDAO();
+        List<Student> studentsList = object.getAllStudents();
+
+        System.out.println("Original Students List ::");
+        studentsList.forEach(System.out::println);
+        System.out.println("----------------------------");
+        List<Student> dummyStudents = object.clone();
+        Student std = new Student();
+        std.setName("vineet");
+        std.setStudendId("1003");
+        System.out.println("Dummy Students List ::");
+        dummyStudents.add(std);
+        dummyStudents.forEach(System.out::println);
+        System.out.println("----------------------------");
+
+        System.out.println("Original Students List ::");
+        studentsList.forEach(System.out::println);
+    }
 }
